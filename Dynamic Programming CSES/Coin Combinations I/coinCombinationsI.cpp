daf2b0a45eed959e13618dcceddc89a246d2526f
vozojk <vojtech.sandera@gmail.com>
2026-04-26 19:55:36 +0200

    Coin Combinations I code

---
+++ Dynamic Programming CSES/Coin Combinations I/coinCombinationsI.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+ 
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+ 
+// --- Constants ---
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+ 
+void solve() {
+ 
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    vector<int> dp(x+1, 0);
+    dp[0]=1;
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+    for (int i = 1; i <= x; i++) {
+        for (int j = 0; j < n; j++) {
+            int val = coins[j];
+            if (i-val<0)continue;
+            dp[i] = (dp[i]+dp[i-val])%MOD;
+        }
+    }
+    cout << dp[x];
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+    int t = 1;
+    //cin >> t;
+ 
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
