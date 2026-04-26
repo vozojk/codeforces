715f356f89a13eba94928ae86a62ecb3531acf21
vozojk <vojtech.sandera@gmail.com>
2026-04-26 19:55:47 +0200

    Removing Digits code

---
+++ Dynamic Programming CSES/Removing Digits/removingDigits.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+ 
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+ 
+// --- Constants ---
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+void solve() {
+ 
+    int n;
+    cin >> n;
+    vector<int> dp(n+1, 1e7);
+ 
+    dp[n]=0;
+    for (int i = n; i >= 0; i--) {
+        int temp = i;
+        while (temp>0){
+            int d = temp%10;
+            if (i-d<0)continue;
+            dp[i-d] = min(dp[i]+1, dp[i-d]);
+            temp/=10;
+        }
+    }
+ 
+    cout << dp[0];
+ 
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+    int t = 1;
+    //cin >> t;
+ 
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
