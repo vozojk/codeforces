e85dc6496bbb05cf7bdd1846a7a680a065e1a145
vozojk <vojtech.sandera@gmail.com>
2026-04-27 17:12:58 +0200

    Grid Paths I code

---
+++ Dynamic Programming CSES/Grid Paths I/gridPathsI.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+ 
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+ 
+ 
+ 
+// --- Constants ---
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+void solve() {
+ 
+    int n;
+    cin >> n;
+    vector<vector<int>> dp(n, vector<int>(n,0));
+    dp[0][0]=1;
+    vector<vector<char>> grid(n, vector<char>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j]=='*') {
+                dp[i][j]=0;
+                continue;
+            }
+ 
+            int top = i>0 ? dp[i-1][j] : 0;
+            int left = j>0 ? dp[i][j-1] : 0;
+            dp[i][j] = (dp[i][j] + top + left)%MOD;
+        }
+    }
+    cout << dp[n-1][n-1];
+ 
+ 
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+    int t = 1;
+    //cin >> t;
+ 
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
