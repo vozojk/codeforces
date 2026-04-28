ef976f751169cca99fc4d60b0c0e553ffb1f6923
vozojk <vojtech.sandera@gmail.com>
2026-04-28 23:38:23 +0200

    Disturbing Distribution code

---
+++ CODEFORCES/10. Div 2 - 28-04-2026/A - Disturbing Distribution/disturbingDistribution.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+
+
+
+// --- Constants ---
+const int MOD = 676767677;
+const ll INF = 1e18;
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void solve() {
+
+    int n, a;
+    cin >> n;
+    ll total = 0;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+        if (nums[i]!=1)total+=nums[i];
+    }
+    if (nums[n-1]==1)total++;
+    cout << total << "\n";
+
+
+
+
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
