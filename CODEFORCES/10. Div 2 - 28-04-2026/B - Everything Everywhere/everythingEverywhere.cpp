128bce8d90704ec4a5370f3efa349e27f23dcfe6
vozojk <vojtech.sandera@gmail.com>
2026-04-28 23:38:34 +0200

    Everything Everywhere code

---
+++ CODEFORCES/10. Div 2 - 28-04-2026/B - Everything Everywhere/everythingEverywhere.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+
+
+
+// --- Constants ---
+const int MOD = 676767677;
+const ll INF = 1e18;
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void solve() {
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)cin >> arr[i];
+    int count = 0;
+    for (int i = 0; i < n-1; i++) {
+        if (abs(arr[i]-arr[i+1])==gcd(arr[i], arr[i+1]))count++;
+    }
+    cout << count << "\n";
+
+
+
+
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
