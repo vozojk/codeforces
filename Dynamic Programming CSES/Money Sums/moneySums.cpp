2f0ede5a78b21dc3e1ab281024dc037a3a19f576
vozojk <vojtech.sandera@gmail.com>
2026-05-01 22:09:42 +0200

    Money Sums code

---
+++ Dynamic Programming CSES/Money Sums/moneySums.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+ 
+ 
+ 
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+ 
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+ 
+void solve(){
+    //Unique Sets, Single Use: Outer = Items, Inner = Backwards (0/1 Knapsack)
+ 
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    int m = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+        m = max(m, c[i]);
+    }
+    vector<int> dp(n*m+1, 0);
+    dp[0]=1;
+    for (int i = 0; i < n; i++) {
+        for (int j = n*m; j >= 0; j--) {
+            if (j-c[i]<0 || dp[j]==1)continue;
+            dp[j]=dp[j-c[i]]>0 ? 1 : 0;
+        }
+    }
+    int k = 0;
+    for (int a : dp) {
+        if (a>0) {
+            k++;
+        }
+    }
+    cout << k-1 << "\n";
+    for (int i = 1; i <= n*m; i++) {
+        if (dp[i]>0) {
+            cout << i << " ";
+        }
+    }
+ 
+ 
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+ 
+    int t = 1;
+    //cin >> t;
+ 
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
