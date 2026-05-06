b13a3d435a81e23694bfee8e464132162fe9bca7
vozojk <vojtech.sandera@gmail.com>
2026-05-06 23:54:02 +0200

    Zhily and Mex and Max code

---
+++ CODEFORCES/12. Div 2 - 06-05-2026/B - Zhily and Mex and Max/zhilyAndMexAndMax.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    vector<ll> nums(n);
+    ll m = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+        m = max(m, nums[i]);
+    }
+
+    sort(nums.begin(), nums.end());
+    ll mex = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i]==mex) {
+            mex++;
+        }
+    }
+
+    ll sum = n*m + (mex-2)*(mex-1)/2 + mex + mex*(n-mex);
+    ll sum2 = m*n + mex*(mex+1)/2 + mex*(n-mex-1);
+
+    cout << (m < mex ? sum : sum2) << "\n";
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
