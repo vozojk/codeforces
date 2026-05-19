b6c5d54aa976650cb92a9cf095621b95514816da
vozojk <vojtech.sandera@gmail.com>
2026-05-19 02:11:20 +0200

    Optimal Purchase code

---
+++ CODEFORCES/14. Div 3 - 19-05-2026/A. Optimal Purchase/optimalPurchase.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void solve() {
+
+    ll a,b,c;
+    cin >> a >> b >> c;
+    ll total = 0;
+    bool c3 = 3*b>c;
+    bool c2 = 2*b>c;
+    bool c1 = b>c;
+    if (c3 && a>2) {
+        total += c*(a/3);
+        a = a%3;
+    }
+    if (c2 && a>1) {
+        total += c;
+        a = a-2;
+    }
+    if (c1) {
+        total += a*c;
+    }else {
+        total += a*b;
+    }
+    cout << total << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
