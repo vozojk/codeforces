630b37df0bef53a46e58f4c3a08f4804ac26ab49
vozojk <vojtech.sandera@gmail.com>
2026-05-23 01:06:38 +0200

    Sliding Window Distinct Values code

---
+++ Sliding Window CSES/Sliding Window Distinct Values/slidingWindowDistinctValues.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+ 
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+ 
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+ 
+void solve(){
+    int n,k,x;
+    cin >> n >> k;
+    unordered_map<int,int> c;
+    c.reserve(1e5);
+    int distinct = 0;
+    queue<int> q;
+    for (int i = 0; i < k; i++) {
+        cin >> x;
+        q.push(x);
+        if (c[x]==0)distinct++;
+        c[x]++;
+    }
+    for (int i = k; i < n; i++) {
+        cout << distinct << " ";
+        if (c[q.front()]==1)distinct--;
+        c[q.front()]--; q.pop();
+        cin >> x;
+        q.push(x);
+        if (c[x]==0)distinct++;
+        c[x]++;
+    }
+    cout << distinct;
+ 
+ 
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t = 1;
+ 
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
