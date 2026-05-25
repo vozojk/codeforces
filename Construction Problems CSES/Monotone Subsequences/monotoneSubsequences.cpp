6d99d49ff37b36572631c2b604fb5c6cd7b048bc
vozojk <vojtech.sandera@gmail.com>
2026-05-26 01:30:39 +0200

    Monotone Subsequences code

---
+++ Construction Problems CSES/Monotone Subsequences/monotoneSubsequences.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+ 
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+ 
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+ 
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+ 
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+ 
+ 
+ 
+ 
+void solve(){
+ 
+    int n,k;
+    cin >> n >> k;
+    if (n > k*k) {
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+    int left = 1;
+    int right = k;
+    while (right < n) {
+        for (int i = right; i >= left; i--) {
+            cout << i << " ";
+        }
+        left+=k;
+        right+=k;
+    }
+    for (int i = n; i >= left; i--) {
+        cout << i << " ";
+    }
+    cout << "\n";
+ 
+ 
+ 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t = 1;
+ 
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
