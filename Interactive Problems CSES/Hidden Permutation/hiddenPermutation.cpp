08f5c73aa3b038e3d8b49f3d75095fcfc7385cce
vozojk <vojtech.sandera@gmail.com>
2026-05-27 00:59:49 +0200

    Hidden Permutation code

---
+++ Interactive Problems CSES/Hidden Permutation/hiddenPermutation.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+
+
+void solve(){
+
+    int n;
+    cin >> n;
+    vector<int>nums (n);
+    for (int i = 1; i <=n; i++) {
+        nums[i-1]=i;
+    }
+    stable_sort(nums.begin(), nums.end(), [](const int a, const int b) {
+        cout << "? " << a << " " << b << endl;
+        string res;
+        cin >> res;
+        return res[0]=='Y';
+    });
+    vector<int> result(n);
+    for (int i = 1; i <= n; i++) {
+        result[nums[i-1]-1] = i;
+    }
+    cout << "! ";
+    for (int a : result)cout << a << " ";
+    cout << endl;
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
