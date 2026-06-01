26c915e66922dfbd8998b0811bff1786c6f6f1e2
vozojk <vojtech.sandera@gmail.com>
2026-06-01 23:59:50 +0200

    B - Villagers code

---
+++ CODEFORCES/17. Div 2 - 01-06-2025 - Virtual/B - Villagers/villagers.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+void solve(){
+
+    int n;
+    ll total = 0;
+    cin >> n;
+    vector<int> g(n);
+
+    for (int i = 0; i < n; i++)cin >> g[i];
+    sort(g.begin(), g.end(), std::greater());
+    for (int i = 0; i < n-1; i=i+2) {
+        total += g[i];
+    }
+    if (n%2==1)total+=g[n-1];
+    cout << total << "\n";
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
