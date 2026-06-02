0ce2f773ab309ef1192e04b7edf35534b7e2369f
vozojk <vojtech.sandera@gmail.com>
2026-06-03 00:29:36 +0200

    D - Chicken Jockey code

---
+++ CODEFORCES/17. Div 2 - 01-06-2025 - Virtual/D - Chicken Jockey/chickenJockey.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+void solve(){
+
+    int n;
+    cin >> n;
+    vector<int> health(n);
+    for (int i = 0; i < n; i++)cin >> health[i];
+    vector<ll> dp(n+1);
+    dp[0]=0;
+    dp[1]=health[n-1]-1;
+    for (int i = 2; i <= n; i++) {
+        dp[i]=min(dp[i-1]+health[n-i]-(i==n ? 0 : 1), dp[i-2]+health[n-i]+max(0, health[n-i+1]-(n-i+1)));
+    }
+    cout << dp[n] << "\n";
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
