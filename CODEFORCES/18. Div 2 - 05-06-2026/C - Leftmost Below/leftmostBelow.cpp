4e8d95e53e683b2277b60a6f1085e3193a52fee9
vozojk <vojtech.sandera@gmail.com>
2026-06-06 00:53:45 +0200

    C - Leftmost Below code

---
+++ CODEFORCES/18. Div 2 - 05-06-2026/C - Leftmost Below/leftmostBelow.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+void solve(){
+
+    int n;
+    cin >> n;
+    // anything in the range
+    int low = 1e9;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)cin >> nums[i];
+    for (int i = 0; i < n; i++) {
+        if (nums[i]>=2*low) {
+            cout << "NO\n";
+            return;
+        }
+        low = min(low, nums[i]);
+    }
+
+    cout << "YES\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
