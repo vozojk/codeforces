df7903c2a14e83fcfa45f0a0585ba1faad7f2c54
vozojk <vojtech.sandera@gmail.com>
2026-06-09 23:59:33 +0200

    B - Different Distances code

---
+++ CODEFORCES/20. Div 2 - EDU - 09-06-2026/B - Different Distances/differentDistances.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    if (n%2==0) {
+        for (int i = 1; i <= n; i=i+2) {
+            cout << i+1 << " " << i << " " << i << " " << i+1 << " " << i << " " << i+1 << " " << i+1 << " " << i << " ";
+        }
+    }else {
+        for (int i = 1; i <= n-3; i=i+2) {
+            cout << i+1 << " " << i << " " << i << " " << i+1 << " " << i << " " << i+1 << " " << i+1 << " " << i << " ";
+        }
+        cout << n-2 << " " << n-2 << " " << n-1 << " " << n-2 << " " << n-1 << " " << n << " " << n-2 << " " << n << " " << n-1 << " " << n-1 << " " << n << " " << n << " ";
+    }
+    cout << "\n";
+
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
