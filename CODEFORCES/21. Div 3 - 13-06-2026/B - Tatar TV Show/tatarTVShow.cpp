33586664f1b05bf20872c581011d802e6c500285
vozojk <vojtech.sandera@gmail.com>
2026-06-13 01:54:41 +0200

    B - Tatar TV Show code

---
+++ CODEFORCES/21. Div 3 - 13-06-2026/B - Tatar TV Show/tatarTVShow.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void solve() {
+
+    int n,k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+    for (int i = 0; i < k; i++) {
+        int cnt = 0;
+        int ptr = i;
+        while (ptr < n) {
+            if (s[ptr]=='1')cnt++;
+            ptr+=k;
+        }
+        if (cnt%2==1) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+
+
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
