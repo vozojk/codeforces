1d4d1f22aaf83f471bee06be2af2142dbf8a8d05
vozojk <vojtech.sandera@gmail.com>
2026-07-02 02:25:08 +0200

    D - Storming Arasaka code

---
+++ CODEFORCES/23. Div 2 - 29-06-2026/D - Storming Arasaka/stormingArasaka.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 998244353;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+vector<pii> factors(int n) {
+    vector<pii> f;
+    for (int x = 2; x*x <= n; x++) {
+        while (n%x == 0) {
+            if (f.empty()||f.back().first!=x)f.push_back({x,1});
+            else f.back().second++;
+            n /= x;
+        }
+    }
+    if (n > 1) f.push_back({n,1});
+    return f;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pii> res = factors(n);
+    int sum = 0;
+    for (auto & re : res)sum += re.second;
+    cout << sum+res.size()-1 << "\n";
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
