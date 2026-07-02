39491e2eae16cbd27a5283841e2a7f45cf7ebbb7
vozojk <vojtech.sandera@gmail.com>
2026-07-02 02:55:31 +0200

    C - RemovevomeR code

---
+++ CODEFORCES/24. Div 4 - 01-07-2026/C - RemovevomeR/removevomeR.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 998244353;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    bool h01 = false;
+    bool h02 = false;
+    for (int i = 0; i < n-1; i++) {
+        if (s[i]=='0' && s[i+1]=='1')h01=true;
+        if (s[i]=='1' && s[i+1]=='0')h02=true;
+    }
+    if (h01 ^ h02) {
+        cout << 2 << '\n';
+    }else {
+        cout << 1 << '\n';
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
