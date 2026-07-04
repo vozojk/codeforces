0698c806064b0fe4d2e64ab5050e3fa6bd71781f
vozojk <vojtech.sandera@gmail.com>
2026-07-05 01:37:00 +0200

    D - Max Sum OR (Easy Version) code

---
+++ CODEFORCES/25. Div 2 - 02-07-2026/D - Max Sum OR (Easy Version)/maxSumOR(EasyVersion).cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 998244353;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+
+    ll l,r;
+    cin >> l >> r;
+    cout << r*(r+1) << "\n";
+    int cur = r;
+    vector<int> ans(r+1,-1);
+    while (cur>=0) {
+        int num = 1, pwr = 0;
+        while (num<=cur) {
+            num*=2;
+            pwr++;
+        }
+        int mask = (1 << pwr)-1;
+        int lower = cur ^ mask;
+        for (int i = cur; i >= lower; i--) {
+            ans[i]=i^mask;
+        }
+        cur = lower-1;
+    }
+    for (const int a : ans)cout << a << " ";
+    cout << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
