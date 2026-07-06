42eababd1d4bed0ce039f8cebf088a4013d3c8e4
vozojk <vojtech.sandera@gmail.com>
2026-07-06 03:54:55 +0200

    A - Table With Numbers code

---
+++ CODEFORCES/26. Div 2 - 04-07-2026/A - Table With Numbers/tableWithNumbers.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 998244353;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+
+    int n,h,l;
+    cin >> n >> h >> l;
+    vector<int> nums;
+    int cnt = n;
+    int borderline = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a>max(h,l))continue;
+        if (a>min(h,l))borderline++;
+        nums.push_back(a);
+    }
+    int sz = nums.size();
+    int res = -1;
+    if (borderline>sz/2)res = sz-borderline;
+    else res = sz/2;
+    cout << res << "\n";
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
