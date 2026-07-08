6e244931fc8078249608be72bb948d4b3db80361
vozojk <vojtech.sandera@gmail.com>
2026-07-09 01:18:40 +0200

    B - Optimal Shifts code

---
+++ CODEFORCES/28. Div 2 - 07-07-2026/B - Optimal Shifts/optimalShifts.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+
+    int n;
+    string s;
+    cin >> n >> s;
+    int zm = 0;
+    int z = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i]=='0') {
+            z++;
+        }else {
+            zm = max(z, zm);
+            z=0;
+        }
+    }
+    if (s[n-1]=='0') {
+        int idx = 0;
+        while (s[idx]=='0') {
+            z++;
+            idx++;
+        }
+        zm = max(z, zm);
+    }
+    cout << zm << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
