dfcf8ff256ed3bdbf07502447b6e217b8c4206b7
vozojk <vojtech.sandera@gmail.com>
2026-07-10 23:03:54 +0200

    B - Tab Closing code

---
+++ CODEFORCES/29. Div 2 - 09-07-2026/B - Tab Closing/tabClosing.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 998244353;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+// b>a/m -> b/a > 1/m m==a/b
+void solve() {
+    // scr_width, min_tab_width, tabs
+    ll a,b,n;
+    cin >>a>>b>>n;
+    if (a/n>=b || a==b) {
+        cout << 1 << "\n";
+    }else cout << 2 << "\n";
+    // a/m==b -> m=a*b
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
