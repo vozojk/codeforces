a22555a2fc90a44f9ff73ff79ab865b4a342a8c5
vozojk <vojtech.sandera@gmail.com>
2026-07-22 02:22:55 +0200

    B - Yet Another Constructive code

---
+++ CODEFORCES/31. Div 2 - 19-07-2026/B - Yet Another Constructive/yetAnotherConstructive.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+
+using namespace std;
+
+
+void solve() {
+
+    int n,k,m;
+    cin >> n >> k >> m;
+
+    if (k>m) {
+        cout << "no\n";
+        return;
+    }
+    cout << "yes\n";
+    for (int i = 0; i < k-1; i++)cout << 1 << " ";
+    cout << m-k+1 << " ";
+    for (int i = k; i < n; i++)cout << m+1 << " ";
+    cout << "\n";
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
