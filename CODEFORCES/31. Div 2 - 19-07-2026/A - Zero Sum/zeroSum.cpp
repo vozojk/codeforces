161335ad13911604bce133b24fbf6773c9cbe99d
vozojk <vojtech.sandera@gmail.com>
2026-07-22 02:21:12 +0200

    A - Zero Sum code

---
+++ CODEFORCES/31. Div 2 - 19-07-2026/A - Zero Sum/zeroSum.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e9;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+
+using namespace std;
+
+
+void solve() {
+
+    int n;
+    cin >> n;
+    int a;
+    int diff =0;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        diff+=a;
+    }
+    if (diff==0) {
+        cout << "yes\n";
+        return;
+    }
+    if (n==2 || n%2==1 || abs(diff/2)%2==1) {
+        cout << "no\n";
+    }else {
+        cout << "yes\n";
+    }
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
