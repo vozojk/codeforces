13bc7f2d8d61828c049e3288d7ac0c1d95093bd8
vozojk <vojtech.sandera@gmail.com>
2026-07-27 02:43:02 +0200

    A - Notelock code

---
+++ CODEFORCES/33. Div 2 - 24-07-2026/A - Notelock/notelock.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#include <climits>
+#include <string>
+#include <cmath>
+
+// --- Type Aliases ---
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+#ifdef lOCAl
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+//#define dbg(x)
+#endif
+
+// --- Constants ---
+constexpr int MOD = 1e9+7;
+constexpr ll INF = 1e18+1;
+
+// 4-Directional Grid Movement
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const char dir_char[4] = {'U', 'D', 'L', 'R'};
+// For 8-directional movement (Kings/Queens):
+const int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1}, dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#include <iostream>
+
+using namespace std;
+
+
+void solve() {
+
+    int n,k;
+    string s;
+    cin >> n >> k >> s;
+
+    int last1 = -1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i]=='1') {
+            if(i-last1>=k || last1==-1) {
+                cnt++;
+            }
+            last1=i;
+        }
+    }
+    cout << cnt << "\n";
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
